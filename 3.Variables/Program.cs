cf9dd6da4530a3c320c411b259aba61b325594e5
A252431 <A252431@LSBSA-C26NHW3>
2023-12-06 14:04:41 +0200

    Add project files.

---
+++ 3.Variables/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _3.Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaire variables
+            string name = "John";
+            int age;
+
+            //initialization
+            name = "John";
+            age = 10;
+
+            //what will be seen on the console
+            Console.WriteLine("There was once a boy called " + name);
+            Console.WriteLine(name + " was " + age + " years of age");
+            Console.WriteLine(name + " never really liked his name.");
+
+            //change variable values
+            age= 18;
+            name = "Tom";
+
+            Console.WriteLine("At age " + age + " he changed his name to " + name);
+
+            //to freeze the console
+            Console.ReadLine();
+        }
+    }
+}
